e68416701523b7726aac5c379767773b32b0e73d
MANISH DHATRAK <98075143+astromanu007@users.noreply.github.com>
2024-06-22 18:22:27 +0530

    Add files via upload

---
+++ Virtual_Mouse.py
@@ -0,0 +1,41 @@
+#This Code is sucessfully completed by Mr.Manish
+
+import cv2  # This library is used for camera
+import mediapipe as mp # This library is used for sensing hand gestures
+import pyautogui # This library is used for fingers calibaration
+cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
+index_y = 0
+while True:
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    frame_height, frame_width, _ = frame.shape
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+    if hands:
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame, hand)
+            landmarks = hand.landmark
+            for id, landmark in enumerate(landmarks):
+                x = int(landmark.x*frame_width)
+                y = int(landmark.y*frame_height)
+                if id == 8:
+                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
+                    index_x = screen_width/frame_width*x
+                    index_y = screen_height/frame_height*y
+
+                if id == 4:
+                    cv2.circle(img=frame, center=(x,y), radius=10, color=(0, 255, 255))
+                    thumb_x = screen_width/frame_width*x
+                    thumb_y = screen_height/frame_height*y
+                    print('outside', abs(index_y - thumb_y))
+                    if abs(index_y - thumb_y) < 20:
+                        pyautogui.click()
+                        pyautogui.sleep(1)
+                    elif abs(index_y - thumb_y) < 100:
+                        pyautogui.moveTo(index_x, index_y)
+    cv2.imshow('Gesture Control Mouse', frame)
+    cv2.waitKey(1)
